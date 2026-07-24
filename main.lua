f1f14a67b2de22c309003113f2985672656d1d36
renandeivison <renandeivison@gmail.com>
2026-07-24 16:01:06 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -25,7 +25,7 @@ local Screen = Device.screen
 local Dispatcher = require("dispatcher")
 local LuaSettings = require("luasettings")
 local T = require("ffi/util").template
-local PLUGIN_VERSION = "1.2.0"
+local PLUGIN_VERSION = "1.3.0"
 
 local QuickSettingsPlugin = WidgetContainer:extend{
     name = "quicksettings",
